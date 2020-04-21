0e42b688b0eb33ecb260a2dc65bcd3b3eeb94470
Samael Newgate <39318392+samnewga@users.noreply.github.com>
2020-04-21 14:51:05 -0700

    Update db.js

--- pern-todo/server/db.js
+++ pern-todo/server/db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "ManaNewgate90",
+  //replace with postgresql user
+  user: "USER",
+  //replace with postgresql password
+  password: "PASSWORD",
   host: "localhost",
   port: 5432,
-  database: "perntodo",
+  //replace with postgresql database
+  database: "DATABASE",
 });
 
 module.exports = pool;
